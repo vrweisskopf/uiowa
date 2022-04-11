4a26ff92e94a443726a3197e3614485524d7cd53
Vanessa Weisskopf <vanessa-weisskopf@uiowa.edu>
2022-04-10 22:22:13 -0500

    added branding bar & set up routing

--- src/app/components/branding-bar/branding-bar.component.spec.ts
+++ src/app/components/branding-bar/branding-bar.component.spec.ts
@@ -1,32 +1,18 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
-import { BrandingBarComponent } from './branding-bar.component';
+import { BrandingBarComponent } from "./branding-bar.component";
 
-describe('BrandingBarComponent', () => {
+describe("BrandingBarComponent", () => {
   let component: BrandingBarComponent;
   let fixture: ComponentFixture<BrandingBarComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ BrandingBarComponent ],
-      imports: [
-        NoopAnimationsModule,
-        ReactiveFormsModule,
-        MatButtonModule,
-        MatCardModule,
-        MatInputModule,
-        MatRadioModule,
-        MatSelectModule,
-      ]
+      declarations: [BrandingBarComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BrandingBarComponent);
@@ -34,7 +20,7 @@ describe('BrandingBarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should compile', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
